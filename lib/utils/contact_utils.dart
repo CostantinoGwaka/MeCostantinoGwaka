1253ad171fde6047e94627cb7f15c3ac79e8baec
Costantino Dionis Gwaka <gwaka94@gmail.com>
2023-08-24 12:16:33 +0300

    fix contact

--- lib/utils/contact_utils.dart
+++ lib/utils/contact_utils.dart
@@ -14,8 +14,8 @@ class ContactUtils {
   ];
 
   static const List<String> details = [
-    "Attock, Pakistan",
-    "(+92) 346 0159889",
-    "hamza.6.shakeel@gmail.com",
+    "Dodoma, Tanzania",
+    "(+255) 659 515 042",
+    "gwaka94@gmail.com",
   ];
 }
